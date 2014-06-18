4a44658cad9033f7b435567644c9366e7f0c5218
Matt Haggard <haggardii@gmail.com>
2014-06-18 14:10:56 -0600

    webpath run does things

--- webpath/script.py
+++ webpath/script.py
@@ -1,11 +1,74 @@
 from twisted.internet import task, defer
 from twisted.python import usage
 
+import sys
+
+from webpath.runner import basicRunner, Context
+
+
+
+class Serializer(object):
+
+
+    def load_yaml(self, fh):
+        import yaml
+        return yaml.load(fh.read())
+
+
+    def dump_yaml(self, data, fh):
+        import yaml
+        fh.write(yaml.dump(data, default_flow_style=False))
+        fh.flush()
+
+
+    def load_json(self, fh):
+        import json
+        return json.loads(fh.read())
+
+
+    def dump_json(self, data, fh):
+        import json
+        fh.write(json.dumps(data, indent=4))
+        fh.flush()
+
 
 
 class RunOptions(usage.Options):
 
     synopsis = 'Run a set of steps'
+
+    optParameters = [
+        ('input', 'i', None, "Input filename (default stdin)"),
+        ('input-format', 'f', "yaml", "Input format"),
+        ('output', 'o', None, "Output filename (default stdout)"),
+        ('output-format', 'F', "yaml", "Output format"),
+    ]
+
+
+    @defer.inlineCallbacks
+    def doCommand(self, options, global_options):
+        ifh = sys.stdin
+        if options['input']:
+            ifh = open(options['input'], 'rb')
+
+        ofh = sys.stdout
+        if options['output']:
+            ofh = open(options['output'], 'wb')
+
+
+        serializer = Serializer()
+        load = getattr(serializer, 'load_' + options['input-format'])
+
+        actions = load(ifh)
+
+        context = Context()
+        runner = basicRunner()
+
+        result = yield runner.runActions(actions, context)
+
+        dump = getattr(serializer, 'dump_' + options['output-format'])
+        dump(result, ofh)
+
 
 
 class Options(usage.Options):
@@ -18,9 +81,9 @@ class Options(usage.Options):
 def main(reactor, args):
     options = Options()
     options.parseOptions(args[1:])
-    return defer.succeed('hey')
+    subOptions = options.subOptions
+    return defer.maybeDeferred(subOptions.doCommand, subOptions, options)
 
 
 def run():
-    import sys
     task.react(main, [sys.argv])
